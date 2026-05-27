c0b54b640cc809ad966b96be586bac5e8c9f426a
PARTHO SARKER <125728613+partho202@users.noreply.github.com>
2026-05-28 01:02:48 +0600

    Time: 0 ms (100.00%) | Memory: 23.9 MB (54.88%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        
+        int n = nums.size();
+        int pos = 0;
+
+        for(int i = 0 ; i < n ; i++){
+            if(nums[i] != 0){
+                nums[pos] = nums[i];
+                pos++;
+            }
+        }
+
+        for(int i = pos ; i < n ; i++){
+            nums[i] = 0;
+        }
+    }
+};
