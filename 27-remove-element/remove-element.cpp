766a111725ad1ff514e9aa54f23407d917d16852
PARTHO SARKER <125728613+partho202@users.noreply.github.com>
2026-05-29 10:54:07 +0600

    Time: 0 ms (100.00%) | Memory: 11.7 MB (83.33%) - LeetSync

---
+++ 27-remove-element/remove-element.cpp
@@ -0,0 +1,16 @@
+//-----------राधा-----------//
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        
+        int pos = 0 ;
+
+        for(int i = 0 ; i< nums.size(); i++){
+            if(nums[i]!= val){
+                nums[pos] = nums[i];
+                pos++;
+            }
+        }
+        return pos;
+    }
+};
