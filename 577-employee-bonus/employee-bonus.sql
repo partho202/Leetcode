7389127f285970b2c76ffe05c215809cfd524504
PARTHO SARKER <125728613+partho202@users.noreply.github.com>
2026-05-31 21:18:48 +0600

    Time: 994 ms (52.22%) | Memory: 0B (100.00%) - LeetSync

---
+++ 577-employee-bonus/employee-bonus.sql
@@ -0,0 +1,7 @@
+-- //-----------राधा-----------//
+Select Employee.name ,  Bonus.bonus
+from Employee
+left join Bonus
+on  Employee.empId = Bonus.empId
+where Bonus.bonus < 1000
+or Bonus.bonus is null;
