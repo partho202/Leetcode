ab0840787bfac98f2b0aed2c007f9159960e9af2
PARTHO SARKER <125728613+partho202@users.noreply.github.com>
2026-06-16 01:29:37 +0600

    Time: 623 ms (83.53%) | Memory: 0B (100.00%) - LeetSync

--- 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
+++ 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 -- //-----------राधा-----------//
-select user_id, max(time_stamp)  as last_stamp 
+select user_id , max(time_stamp) as last_stamp 
 from Logins
 where year(time_stamp) = 2020
-group by user_id
+group by user_id 
