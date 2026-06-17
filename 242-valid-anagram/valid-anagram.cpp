3974a24fdeea8f8a21ac9c9de59377f34aff6ccc
PARTHO SARKER <125728613+partho202@users.noreply.github.com>
2026-06-17 10:16:55 +0600

    Time: 2 ms (50.51%) | Memory: 9.8 MB (49.27%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,15 @@
+//-----------राधा-----------//
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        if(s.size()!=t.size()){
+            return false;
+        }
+
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+
+        return s==t;
+    }
+};
