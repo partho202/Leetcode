dc0bc884d9e617845df0dc2024da0a5b70e0150e
PARTHO SARKER <125728613+partho202@users.noreply.github.com>
2026-06-18 22:16:18 +0600

    Time: 7 ms (19.18%) | Memory: 9.7 MB (49.23%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,10 +3,9 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         
-        if(s.size()!=t.size()){
+        if(s.length()!=t.length()){
             return false;
         }
-
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
 
