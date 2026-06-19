0ec33c3c2ab13c8503925ab7b54a3d6d27bc198b
PARTHO SARKER <125728613+partho202@users.noreply.github.com>
2026-06-20 02:20:53 +0600

    Time: 2 ms (20.32%) | Memory: 22.3 MB (8.52%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n = nums.size() ;
+        
+        int n = nums.size();
 
-        vector <int> freq( n + 1 , 0 );
+        vector<int>freq(n+1,0); // first all number are zero
 
-        for(int i = 0 ; i < n ; i++ ){
-            freq[nums[i]] = 1;
+        for(int i = 0 ; i < n ; i++){
+            freq[nums[i]]=1;
         }
         for(int i = 0 ; i <= n ; i++){
             if(freq[i]==0){
-                return i;
+                return i ;
             }
         }
         return -1;
