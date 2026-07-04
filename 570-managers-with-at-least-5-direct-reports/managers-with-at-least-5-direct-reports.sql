b15b3ca731ae12ae3fecc21a06ba0021d8420ece
PARTHO SARKER <125728613+partho202@users.noreply.github.com>
2026-07-04 14:53:56 +0600

    Time: 347 ms (77.09%) | Memory: 0B (100.00%) - LeetSync

---
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+-- //-----------राधा-----------//
+select name
+from Employee
+where id in ( select managerId
+    from Employee
+    where managerId is not null
+    group by managerId
+    having count(managerId)>=5);
