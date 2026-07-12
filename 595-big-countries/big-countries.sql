2966c98d9000e7dcaa4246e2e3a7db872fb8e388
PARTHO SARKER <125728613+partho202@users.noreply.github.com>
2026-07-12 13:50:45 +0600

    Time: 298 ms (60.88%) | Memory: 0B (100.00%) - LeetSync

--- 595-big-countries/big-countries.sql
+++ 595-big-countries/big-countries.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
 -- //-----------राधा-----------//
-select name,population ,area    
-from world
-where area >= 3000000 or 
-population >= 25000000
+select name,population,area
+from World
+where area >=3000000 or population >= 25000000;
