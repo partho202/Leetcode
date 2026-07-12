0e51de6f08f32e549298d0cae3c4cb3d93d49602
PARTHO SARKER <125728613+partho202@users.noreply.github.com>
2026-07-12 14:14:21 +0600

    Time: 449 ms (38.73%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,6 +1,7 @@
 # Write your MySQL query statement below
 -- //-----------राधा-----------//
-select distinct author_id as id
+select author_id as id
 from Views
-where author_id = viewer_id 
-order by author_id ASC
+where author_id = viewer_id
+group by author_id
+order by author_id asc;
