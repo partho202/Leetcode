325c4cf68602ad9ea0c460f60d0ad03724e3e344
PARTHO SARKER <125728613+partho202@users.noreply.github.com>
2026-07-18 01:53:21 +0600

    Time: 0 ms (100.00%) | Memory: 9.6 MB (81.81%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
@@ -1,14 +1,15 @@
-//-----------राधा-----------// 
+//-----------राधा-----------//
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        if( str1+ str2 != str2 + str1){ // and here if str1+ str2 and str2 + str1 not equall to then it's simply print empty string because not concatined more times..
+        
+        if((str1+str2) != (str2+str1)){ // here check two string index matching 
             return "";
         }else{
-            return str2.substr(0,gcd(str1.size(),str2.size()));
-            // here you can write st1.sbustr() or str2.substr() and substr() is a function of string and print length of the string like str1 = "AAAB" and use str1.substr(0,2) the print "AAA"
+            return str1.substr(0,gcd(str1.size(),str2.size()));
         }
 
     }
 };
 
+//tc-04 str1+str2 = AAAAABAAA then str2+str1 = AAAAAAAAB so here two string index not matching that's why return "" empty  string
