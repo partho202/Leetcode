81cdc7331e6cdae14d348ec265c9dee7c4f3e5ee
PARTHO SARKER <125728613+partho202@users.noreply.github.com>
2026-07-18 01:09:06 +0600

    Time: 2 ms (43.76%) | Memory: 8.6 MB (31.50%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -1,20 +1,26 @@
-//-----------राधा-----------// 
+//-----------राधा-----------//
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
         
-        string marged="";
+        string result = "";
+        
         int i = 0;
-        while( i < word1.size() || i < word2.size() ){
-           
-            if(i < word1.size()){
-                marged+=word1[i];
+        
+        string merg = word1 + word2 ;
+        
+        int n = merg.length(); // 6(for-tes1)
+        
+        while(n > i){
+            
+            if(i < word1.length()){
+                result+=word1[i]; //0: "" + a now result = a; 1: ap+b result = apb 2: apbq+c result = apbqc
             }
-            if(i < word2.size()){
-                marged+=word2[i];
+            if(i< word2.length()){
+                result+=word2[i]; // 0: a + p now result = ap; 1: apb + q result = apbq 2: apbqc+r result = apbqcr
             }
-            i++;
+            i++; //0 1 2 3 4 5 6(break the loop when i = 6)
         }
-        return marged;
+        return result;
     }
 };
