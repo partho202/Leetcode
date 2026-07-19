141405839c4a4af5886d30d1d6a9ba8a7963f09c
PARTHO SARKER <125728613+partho202@users.noreply.github.com>
2026-07-19 23:58:58 +0600

    Time: 3 ms (10.96%) | Memory: 24.2 MB (45.02%) - LeetSync

---
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -0,0 +1,26 @@
+//-----------राधा-----------//
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        
+        int count = 0 ;
+        int size = flowerbed.size();
+
+        for(int i = 0 ; i < size ;i++){
+            
+            if(flowerbed[i]==0){
+
+                bool left = (i==0) || (flowerbed[i-1]==0);
+                bool right = (i== size-1) || (flowerbed[i+1]==0) ; // here left and right check because adjacent flowers can't planned
+                
+                if(left&&right){
+                    flowerbed[i]=1;
+                    count++;
+                }
+            } 
+        }
+        
+        return count>=n ;
+
+    }
+};
